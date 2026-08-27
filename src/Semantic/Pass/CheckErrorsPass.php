e5cefd5954aef5dec4dfac89b7e12f22d440801d
Andrew Masiye <amasiye313@gmail.com>
2026-08-27 11:11:02 +0200

    feat(transpilation): rename CheckBindings, CheckErrors, CheckGenerics, CheckTypes, and DeclareSymbols classes to include 'Pass' suffix; add new classes for transpilation

--- src/Semantic/Pass/CheckErrorsPass.php
+++ src/Semantic/Pass/CheckErrorsPass.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace Amasiye\Phplus\Semantic\Pass;
 
-class DeclareSymbols
+class CheckErrorsPass
 {
 }
--- src/Semantic/Pass/ResolveNamesPass.php
+++ src/Semantic/Pass/ResolveNamesPass.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace Amasiye\Phplus\Semantic\Pass;
 
-class CheckGenerics
+class ResolveNamesPass
 {
 }
